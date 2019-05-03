7faf69df49d2f53b234c02d335dad6ef532d2c35
Lucho-A <lucho@lucho-L>
2019-05-02 23:57:23 -0300

    Primera version. P0001 OK

---
+++ Rosalind/Headers/libRosalind.h
@@ -0,0 +1,26 @@
+/*
+ * libRosalind.h
+ *
+ *  Created on: 2 may. 2019
+ *      Author: lucho
+ */
+
+#ifndef LIBROSALIND_H_
+#define LIBROSALIND_H_
+//includes
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <time.h>
+    #include <math.h>
+    #include <string.h>
+//defines
+    #define FALSE 0
+    #define TRUE 1
+	#define PI 3.14159265358979323846264338327950288419716939937510
+//typedef
+	typedef int bool;
+	typedef long double ld;
+    typedef long unsigned int lu;
+    typedef long long unsigned int llu;
+
+#endif /* LIBROSALIND_H_ */
---
+++ Rosalind/Rosalind.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Rosalind.c
+ Author      : L.
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "libRosalind.h"
+
+int main(void) {
+	P0001();
+	return EXIT_SUCCESS;
+}
